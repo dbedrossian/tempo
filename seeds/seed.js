5df051db4b99a270f473a7655bfe8266433445ac
Chelsea Runacres <chelsea.runacres@gmail.com>
2023-01-03 11:52:01 -0800

    console logs removed

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,7 +7,6 @@ const postData = require('./postData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true
