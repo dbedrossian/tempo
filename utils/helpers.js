4f87dcd7babdf785de91f727a82da0863d2ad077
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-05 17:46:41 -0500

    Added media content

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,4 +7,23 @@ module.exports = {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     return date.toLocaleDateString(undefined, options);
   },
+  ifEquals: (a, b, options) => {
+    if (a === b) {
+      return options.fn(this);
+      }
+    
+    return options.inverse(this);
+  },
+  // checkMedia: (media_type, media_url, alt_text="") => {
+  //   if(media_type === 'image') {
+  //     // let imgEl = document.createElement('img');
+  //     // imgEl.src = media_url;
+  //     // imgEl.alt = alt_text;
+  //     // return imgEl;
+  //     let imgEl = $('<img></img>')
+  //     imgEl.attr('src', media_url);
+  //     imgEl.attr('alt', alt_text);
+  //     return imgEl.html();
+  //   }
+  // }
 };
